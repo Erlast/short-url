e73732ed6398708431b8a7b339728d4ed33b4259
JaneA <bunny9@bk.ru>
2024-05-01 15:36:25 +0300

    iter5

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,11 +2,18 @@ package config
 
 import (
 	"flag"
+	"github.com/caarlos0/env/v11"
+	"log"
 )
 
 type Cfg struct {
 	FlagRunAddr string
 	FlagBaseURL string
+}
+
+type envCfg struct {
+	runAddr string `env:"SERVER_ADDRESS"`
+	baseURL string `env:"BASE_URL"`
 }
 
 func ParseFlags() Cfg {
@@ -19,6 +26,22 @@ func ParseFlags() Cfg {
 	flag.StringVar(&Config.FlagBaseURL, "b", "http://localhost:8080", "base URL")
 
 	flag.Parse()
+
+	var cfg envCfg
+
+	err := env.Parse(&cfg)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(cfg.runAddr) != 0 {
+		Config.FlagRunAddr = cfg.runAddr
+	}
+
+	if len(cfg.baseURL) != 0 {
+		Config.FlagBaseURL = cfg.baseURL
+	}
 
 	return Config
 
