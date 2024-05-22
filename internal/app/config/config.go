c79eaef31c77aef4af9c1e02989fe377b26575ec
JaneA <bunny9@bk.ru>
2024-05-22 21:27:03 +0300

    iter9

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -14,9 +14,9 @@ type Cfg struct {
 }
 
 type envCfg struct {
-	RunAddr     string `env:"SERVER_ADDRESS" envDefault:":8080"`
-	BaseURL     string `env:"BASE_URL" envDefault:"http://localhost:8080"`
-	FileStorage string `env:"FILE_STORAGE_PATH" envDefault:"/tmp/short-url-db.json"`
+	RunAddr     string `env:"SERVER_ADDRESS"`
+	BaseURL     string `env:"BASE_URL"`
+	FileStorage string `env:"FILE_STORAGE_PATH"`
 }
 
 const defaultRunAddr = ":8080"
@@ -30,9 +30,9 @@ func ParseFlags() *Cfg {
 		FileStorage: defaultFileStoragePath,
 	}
 
-	flag.StringVar(&config.FlagRunAddr, "a", defaultRunAddr, "port to run server")
-	flag.StringVar(&config.FlagBaseURL, "b", defaultBaseURL, "base URL")
-	flag.StringVar(&config.FileStorage, "f", defaultFileStoragePath, "file storage path")
+	flag.StringVar(&config.FlagRunAddr, "a", config.FlagRunAddr, "port to run server")
+	flag.StringVar(&config.FlagBaseURL, "b", config.FlagBaseURL, "base URL")
+	flag.StringVar(&config.FileStorage, "f", config.FileStorage, "file storage path")
 
 	flag.Parse()
 	cfg := envCfg{}
