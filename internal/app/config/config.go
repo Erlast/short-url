fefb393b28d81a8124d3bb854226ffa6ceda4e63
JaneA <bunny9@bk.ru>
2024-05-22 21:15:25 +0300

    iter9

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -14,40 +14,39 @@ type Cfg struct {
 }
 
 type envCfg struct {
-	runAddr     string `env:"SERVER_ADDRESS" envDefault:":8080"`
-	baseURL     string `env:"BASE_URL" envDefault:"http://localhost:8080"`
-	fileStorage string `env:"FILE_STORAGE_PATH" envDefault:"/tmp/short-url-db.json"`
+	RunAddr     string `env:"SERVER_ADDRESS" envDefault:":8080"`
+	BaseURL     string `env:"BASE_URL" envDefault:"http://localhost:8080"`
+	FileStorage string `env:"FILE_STORAGE_PATH" envDefault:"/tmp/short-url-db.json"`
 }
 
 const defaultRunAddr = ":8080"
 const defaultBaseURL = "http://localhost:8080"
-
-// const defaultFileStoragePath = "/tmp/short-url-db.json"
+const defaultFileStoragePath = "/tmp/short-url-db.json"
 
 func ParseFlags() *Cfg {
 	config := &Cfg{}
 
 	flag.StringVar(&config.FlagRunAddr, "a", defaultRunAddr, "port to run server")
 	flag.StringVar(&config.FlagBaseURL, "b", defaultBaseURL, "base URL")
-	flag.StringVar(&config.FileStorage, "f", "", "file storage path")
+	flag.StringVar(&config.FileStorage, "f", defaultFileStoragePath, "file storage path")
 
 	flag.Parse()
-	var cfg envCfg
+	cfg := envCfg{}
 
 	if err := env.Parse(&cfg); err != nil {
 		log.Fatalf("can't parse")
 	}
 
-	if len(cfg.runAddr) != 0 {
-		config.FlagRunAddr = cfg.runAddr
+	if len(cfg.RunAddr) != 0 {
+		config.FlagRunAddr = cfg.RunAddr
 	}
 
-	if len(cfg.baseURL) != 0 {
-		config.FlagBaseURL = cfg.baseURL
+	if len(cfg.BaseURL) != 0 {
+		config.FlagBaseURL = cfg.BaseURL
 	}
 
-	if len(cfg.fileStorage) != 0 {
-		config.FileStorage = cfg.fileStorage
+	if len(cfg.FileStorage) != 0 {
+		config.FileStorage = cfg.FileStorage
 	}
 
 	return config
