663acd6528c89bd296a7d0f8240855e5965eb3eb
Jane A <bunny9@bk.ru>
2024-06-01 14:21:22 +0300

    iter11

--- internal/app/storages/pg_storage.go
+++ internal/app/storages/pg_storage.go
@@ -30,15 +30,32 @@ short_url VARCHAR(10) NOT NULL,
 }
 
 func (pgs *PgStorage) SaveURL(id string, originalURL string) error {
+	_, err := pgs.db.Exec("INSERT INTO short_urls(short_url, original_url) VALUES ($1, $2)", id, originalURL)
+	if err != nil {
+		return fmt.Errorf("unable to save url: %w", err)
+	}
 	return nil
 }
 
 func (pgs *PgStorage) GetByID(id string) (string, error) {
-	return "", nil
+	var originalURL string
+	err := pgs.db.QueryRow("SELECT original_url FROM short_urls WHERE short_url = $1", id).Scan(&originalURL)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return "", fmt.Errorf("short URL not found %w", err)
+		}
+		return "", fmt.Errorf("failed to get query: %w", err)
+	}
+	return originalURL, nil
 }
 
 func (pgs *PgStorage) IsExists(key string) bool {
-	return true
+	var count int
+	err := pgs.db.QueryRow("SELECT count(original_url) FROM short_urls WHERE short_url = $1", key).Scan(&count)
+	if err != nil {
+		_ = fmt.Errorf("failed to get query: %w", err)
+	}
+	return count != 0
 }
 
 func (pgs *PgStorage) CheckPing() error {
